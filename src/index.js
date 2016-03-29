7da75dd8866eb0f44b928d4186b70f1224df4db4
Zach Eagle <mazaxakep@gmail.com>
2016-03-29 11:21:54 +0300

    smaller images

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function search (query) {
           type: 'article',
           title: `${movie.nameRU} (${movie.year})`,
           description: movie.description.replace(/\((.*)\)/, ' $&'),
-          thumb_url: `http://st.kp.yandex.net/images/film_big/${movie.id}.jpg`,
+          thumb_url: `http://st.kp.yandex.net/images/film_iphone/iphone360_${movie.id}.jpg`,
           message_text: `http://www.kinopoisk.ru/film/${movie.id}/`,
           parse_mode: 'Markdown'
         }))
